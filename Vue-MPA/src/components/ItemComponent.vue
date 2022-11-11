f164da6420d95669bb7a4b4c57f8dc06d75a1626
AlexanderFransson7 <70211809+festusOp7@users.noreply.github.com>
2022-11-11 13:47:50 +0100

    item

---
+++ Vue-MPA/src/components/ItemComponent.vue
@@ -0,0 +1,9 @@
+<template>
+    <h1>Item one</h1>
+</template>
+<script setup lang="ts">
+
+</script>
+<style scoped>
+
+</style>
--- Vue-MPA/src/routers/index.ts
+++ Vue-MPA/src/routers/index.ts
@@ -1,13 +1,15 @@
 import {createRouter, createWebHashHistory} from 'vue-router';
-import Menu from './views/Menu.vue';
-import Todo from './views/Todo.vue';
+import Menu from '../views/MenuView.vue';
+import Todo from '../views/TodoView.vue';
 
 const routes = [
     {name:'Menu', path: '/', component: Menu},
-    {name: 'Todo', path: '/todo', component: Todo},
+    {name: 'Todo', path: '/todos', component: Todo},
 ];
 
 const router = createRouter({
     history: createWebHashHistory(),
     routes,
 })
+
+export default router;
