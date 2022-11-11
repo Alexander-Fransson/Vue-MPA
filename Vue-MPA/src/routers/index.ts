eedd2b91ba0ee419b114b5cff883a42ec968e780
AlexanderFransson7 <70211809+festusOp7@users.noreply.github.com>
2022-11-11 12:38:54 +0100

    ändrar namn

--- Vue-MPA/src/routers/index.ts
+++ Vue-MPA/src/routers/index.ts
@@ -1,8 +1,10 @@
 import {createRouter, createWebHashHistory} from 'vue-router';
 import Menu from './views/Menu.vue';
+import Todo from './views/Todo.vue';
 
 const routes = [
-    {name:'Menu', path: '/', component: Menu}
+    {name:'Menu', path: '/', component: Menu},
+    {name: 'Todo', path: '/todo', component: Todo},
 ];
 
 const router = createRouter({
