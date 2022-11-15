dfe5c72dfb3034c699ad45016ea2addce504ad24
AlexanderFransson7 <70211809+festusOp7@users.noreply.github.com>
2022-11-15 15:45:22 +0100

    börjar skapa post logik

---
+++ frontend/src/components/AddItemComponent.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+    import { useItemStore } from '../stores/ItemStore';
+    import { ref } from 'vue';
+
+    const store = useItemStore();
+    const newItem = ref('');
+</script>
+
+<template>
+    <form @submit.prevent="store.add({name:newItem, done:false, hidden:false})">
+        <input v-model="newItem" type="text"/>
+        <input type="submit" value="Add">
+    </form>
+</template>
+
+<style scoped>
+
+</style>
