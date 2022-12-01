422e06e15765cc5299ed3de4de3182c6a21a376a
AlexanderFransson7 <70211809+festusOp7@users.noreply.github.com>
2022-12-01 13:21:55 +0100

    skapar todo item component

---
+++ frontend/src/components/TodoItem.vue
@@ -0,0 +1,44 @@
+<script setup lang="ts">
+    defineProps<{
+        id: number
+        name: string
+        done: boolean
+    }>()
+
+    const remove = () => {
+        console.log('delete');
+    }
+
+    const complete = () => {
+        console.log('completed')
+    }
+</script>
+
+<template>
+    <section class="container">
+        <p>{{name}}</p>
+        <q-btn 
+            @click="complete()"
+            label="Y"
+            color="green"
+        />
+        <q-btn
+            @click="remove()" 
+            label="X"
+            color="red"
+        />
+    </section>
+</template>
+
+<style scoped>
+    .container{
+        display: flex;
+        flex-direction: row;
+        margin: auto;        
+    }
+    p{
+        font-size: large;
+        padding: 1vw;
+        margin: 0vh;
+    }
+</style>
