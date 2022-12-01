a466f4a3ece8bb0bb69c44c26cf202924e55568d
AlexanderFransson7 <70211809+festusOp7@users.noreply.github.com>
2022-12-01 09:56:23 +0100

    connected store to backend

--- TodoApi/Models/Todoitem.cs
+++ TodoApi/Models/Todoitem.cs
@@ -5,5 +5,4 @@ public class TodoItem
     public long Id {get; set;}
     public string? Name {get; set;}
     public bool Done {get; set;}
-    public bool Hidden {get; set;}
 }
---
+++ frontend/src/stores/TodoStore.ts
@@ -0,0 +1,52 @@
+import { defineStore } from 'pinia'
+
+interface ITodo {
+    id: number,
+    name: string,
+    done: string
+}
+
+export const useTodoStore = defineStore('TodoStore', {
+    state: () => {
+        return {
+            thingsTodo: [] as ITodo[],
+        }
+    },
+    getters: {
+        async getNewId(): Promise<number> {
+            const req = await fetch('http://localhost:5097/api/TodoItems')
+            const res = await req.json()
+            this.thingsTodo = res;
+            return res.length + 1
+        }
+    },
+    actions: {
+        async refillStore() {
+            const req = await fetch('http://localhost:5097/api/TodoItems')
+            const res = await req.json()
+            this.thingsTodo = res;
+            console.log(JSON.stringify(this.thingsTodo))
+        },
+        async addThingTodo(thingTodo: string) {
+
+            const body = {
+                id: this.getNewId,
+                name: thingTodo,
+                done: false
+            }
+
+            const req = await fetch('http://localhost:5097/api/TodoItems',{
+                method: 'POST',
+                headers: {
+                    'accept':'text/plain',
+                    'Content-Type':'application/json'
+                },
+                body: JSON.stringify(body)
+            })
+
+            const res = await req.json()
+            console.log(res)
+            this.refillStore()
+        }
+    }
+})
